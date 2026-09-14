53a7e992bf86bbe0a4437b66ae68f71bef09d9cf
ggzin694 <gabrielrab1985@gmail.com>
2026-09-14 15:16:43 -0300

    Add voice interaction session class
    Add the minimal session implementation used by the voice interaction service.

---
+++ android/app/src/main/java/com/ggzin694/personalcyberai/CyberVoiceInteractionSession.java
@@ -0,0 +1,10 @@
+package com.ggzin694.personalcyberai;
+
+import android.content.Context;
+import android.service.voice.VoiceInteractionSession;
+
+public class CyberVoiceInteractionSession extends VoiceInteractionSession {
+    public CyberVoiceInteractionSession(Context context) {
+        super(context);
+    }
+}
